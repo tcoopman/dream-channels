4a3712761f605a5463c30152c7fc47523ec94c0b
Thomas Coopman <thomas.coopman@gmail.com>
2021-06-30 21:18:29 +0200

    extra tests

--- test/integration/client-tests.js
+++ test/integration/client-tests.js
@@ -1,16 +1,50 @@
 const WebSocket = require('ws');
 
+class TestWs {
+  constructor() {
+    this.ws = new WebSocket('ws://localhost:8080/ws');
+    this.ws.on('open', () => {
+      this.ws.send('join|chat:1|hi');
+    });
+  }
+  onMessage(fun) {
+    this.ws.on('message', fun)
+  }
+
+  send(data) {
+    this.ws.send(data);
+  }
+
+  terminate() {
+    this.ws.terminate();
+  }
+}
+
+
 test('joining a chat returns the welcome message', (done) => {
-const ws = new WebSocket('ws://localhost:8080/ws');
-
-ws.on('open', function open() {
-  ws.send('join|chat:1|hi');
+  const ws = new TestWs();
+  ws.onMessage((data) => {
+    expect(data).toBe("joined:1|payload:hi");
+    ws.terminate();
+    done();
+  });
 });
 
-ws.on('message', function incoming(data) {
-  expect(data).toBe("joined:1|payload:hi");
-  ws.terminate();
-  done();
-});
+test('chat with multiple', (done) => {
+  const ws1 = new TestWs();
+  const ws2 = new TestWs();
+  const ws3 = new TestWs();
 
+  let counter = 0;
+  ws3.onMessage((data) => {
+    counter++;
+    if (counter == 1) {
+      expect(data).toBe("joined:1|payload:hi");
+      ws3.send('send|chat:1|broadcast');
+    }
+    if (counter == 2) {
+      expect(data).toBe("To everyone");
+      done();
+    }
+  });
 });
--- test/integration/integration.ml
+++ test/integration/integration.ml
@@ -1,29 +1,31 @@
 open Channels
 
-let chat_channel functions topic = Socket.{
-  join = (fun (Payload payload) ->
-    match topic with
-    | WithSubtopic ("chat", chat_id) ->
-        let%lwt () = functions.push @@ "joined:" ^ chat_id ^ "|payload:" ^ payload in
-        [] |> Lwt.return
-    | _ -> [`Stop "invalid topic"] |> Lwt.return);
-  handle_message= (fun (Payload payload) -> 
-    let%lwt () = functions.broadcast_from "To all others" in
-    let%lwt () = functions.broadcast "To everyone" in
-    [`Reply payload] |> Lwt.return);
-}
+let chat_channel functions topic =
+  Socket.
+    { join =
+        (fun (Payload payload) ->
+          match topic with
+          | WithSubtopic ("chat", chat_id) ->
+              let%lwt () = functions.push @@ "joined:" ^ chat_id ^ "|payload:" ^ payload in
+              [] |> Lwt.return
+          | _ ->
+              [ `Stop "invalid topic" ] |> Lwt.return )
+    ; handle_message =
+        (fun (Payload payload) ->
+          match payload with
+          | "broadcast" ->
+              let%lwt () = functions.broadcast "To everyone" in
+              [] |> Lwt.return
+          | _ ->
+              [] |> Lwt.return )
+    }
+
 
 let () =
-  Dream.initialize_log ~level:`Debug ();
+  Dream.initialize_log ~level:`Debug () ;
   Dream.run ~interface:"0.0.0.0"
   @@ Dream.logger
-  @@ Dream.router [
-
-    Dream.get "/ws"
-      (fun _ ->
-        Dream.websocket @@ Socket.channels [
-          ("chat:*", chat_channel);
-          ]);
-
-  ]
+  @@ Dream.router
+       [ Dream.get "/ws" (fun _ -> Dream.websocket @@ Socket.channels [ ("chat:*", chat_channel) ])
+       ]
   @@ Dream.not_found
