5345882932585568b5f91ec2fa8e44c0f30e722b
Thomas Coopman <thomas.coopman@gmail.com>
2021-07-05 13:52:34 +0200

    no need to run the tests serial

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ pkill -f "ava-tests"
 killall integration.exe
 esy x dune exec --root . test/integration/integration.exe &
 sleep 1
-npx ava -sv --timeout=1s
+npx ava -v --timeout=1s
 pkill -f "ava-tests"
 killall integration.exe
