dbb9b6f98eb92200bdaf1da0b1abc82c29e3ce8d
Thomas Coopman <thomas.coopman@gmail.com>
2021-07-05 16:28:44 +0200

    format docs

--- src/socket.mli
+++ src/socket.mli
@@ -5,7 +5,7 @@ type topic =
 type payload = Payload of string
 
 type answer =
-  [ `Ok 
+  [ `Ok
   | `Reply of string
   | `Stop of string
   ]
@@ -21,23 +21,21 @@ type intercept = payload -> bool
 and callbacks =
   { join : functions -> payload -> answer Lwt.t
   ; handle_message : functions -> payload -> answer Lwt.t
-  ; handle_out : payload -> payload option 
+  ; handle_out : payload -> payload option
   ; terminate : unit -> unit
   }
 
 (** A channel
 
-* topic: the exact topic for this channel, for example: chat:public_room
-* intercept: messages that you send to `broadcast` or `broadcast_from` will be checked if they need to be intercepted.
-  When intercept returns true, every broadcasted message will be send to `handle_out`. Giving it an opportunity to transform
-  the message if wanted
-* create_callbacks: creates the callbacks for the given topic
-
-*)
-type channel = {
-  topic: string;
-  intercept: intercept;
-  create_callbacks: topic -> callbacks
-}
+    - topic: the exact topic for this channel, for example: chat:public_room
+    - intercept: messages that you send to `broadcast` or `broadcast_from` will be checked if they
+      need to be intercepted. When intercept returns true, every broadcasted message will be send to
+      `handle_out`. Giving it an opportunity to transform the message if wanted.
+    - create_callbacks: creates the callbacks for the given topic *)
+type channel =
+  { topic : string
+  ; intercept : intercept
+  ; create_callbacks : topic -> callbacks
+  }
 
 val channels : channel list -> Dream.websocket -> unit Lwt.t
