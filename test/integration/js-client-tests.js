30325e098e48ffdae5a11d5478fcacf5faa606b5
Thomas Coopman <thomas.coopman@gmail.com>
2021-07-13 10:46:09 +0200

    make sure that the test can run in parallel

--- test/integration/js-client-tests.js
+++ test/integration/js-client-tests.js
@@ -5,17 +5,25 @@ function end(t, timeout = 100) {
   setTimeout(() => t.end(), timeout);
 }
 
+let ref = 0;
+
+function newChannel() {
+  ref++;
+  return `channel:${ref}`;
+}
+
 test.cb(
   "Connect a channel on a socket - you don't need to wait for the effective connection",
   (t) => {
     t.plan(1);
     const socket = new Socket("ws://localhost:8080/ws");
+    const channelName = newChannel();
 
     socket.connect();
 
-    const channel = socket.channel("channel:1");
+    const channel = socket.channel(channelName);
     channel.join("payload").receive((msg) => {
-      t.is(msg, "reply from channel:1 - your payload was: payload");
+      t.is(msg, `reply from ${channelName} - your payload was: payload`);
     });
     end(t);
   }
@@ -26,13 +34,14 @@ test.cb(
   (t) => {
     t.plan(1);
     const socket = new Socket("ws://localhost:8080/ws");
+    const channelName = newChannel();
 
     socket.connect();
 
     setTimeout(() => {
-      const channel = socket.channel("channel:1");
+      const channel = socket.channel(channelName);
       channel.join("payload").receive((msg) => {
-        t.is(msg, "reply from channel:1 - your payload was: payload");
+        t.is(msg, `reply from ${channelName} - your payload was: payload`);
       });
       end(t);
     }, 100);
@@ -42,16 +51,18 @@ test.cb(
 test.cb("get the reply from the correct channel on join", (t) => {
   t.plan(2);
   const socket = new Socket("ws://localhost:8080/ws");
+  const channelName1 = newChannel();
+  const channelName2 = newChannel();
 
   socket.connect();
 
-  const channel1 = socket.channel("channel:1");
-  channel1.join("payload channel 1").receive((msg) => {
-    t.is(msg, "reply from channel:1 - your payload was: payload channel 1");
+  const channel1 = socket.channel(channelName1);
+  channel1.join("A").receive((msg) => {
+    t.is(msg, `reply from ${channelName1} - your payload was: A`);
   });
-  const channel2 = socket.channel("channel:2");
-  channel2.join("payload channel 2").receive((msg) => {
-    t.is(msg, "reply from channel:2 - your payload was: payload channel 2");
+  const channel2 = socket.channel(channelName2);
+  channel2.join("B").receive((msg) => {
+    t.is(msg, `reply from ${channelName2} - your payload was: B`);
   });
   end(t);
 });
@@ -59,27 +70,47 @@ test.cb("get the reply from the correct channel on join", (t) => {
 test.cb("get the reply from the correct channel on send", (t) => {
   t.plan(6);
   const socket = new Socket("ws://localhost:8080/ws");
+  const channelName1 = newChannel();
+  const channelName2 = newChannel();
 
   socket.connect();
 
-  const channel1 = socket.channel("channel:1");
+  const channel1 = socket.channel(channelName1);
   channel1.join("payload channel 1").receive((msg) => {
-    t.is(msg, "reply from channel:1 - your payload was: payload channel 1");
+    t.is(
+      msg,
+      `reply from ${channelName1} - your payload was: payload channel 1`
+    );
     channel1.push("push 1").receive((msg) => {
-      t.is(msg, "reply on push from channel:1 - your payload was: push 1");
+      t.is(
+        msg,
+        `reply on push from ${channelName1} - your payload was: push 1`
+      );
     });
     channel1.push("push 2").receive((msg) => {
-      t.is(msg, "reply on push from channel:1 - your payload was: push 2");
+      t.is(
+        msg,
+        `reply on push from ${channelName1} - your payload was: push 2`
+      );
     });
     channel1.push("push 3").receive((msg) => {
-      t.is(msg, "reply on push from channel:1 - your payload was: push 3");
+      t.is(
+        msg,
+        `reply on push from ${channelName1} - your payload was: push 3`
+      );
     });
   });
-  const channel2 = socket.channel("channel:2");
+  const channel2 = socket.channel(channelName2);
   channel2.join("payload channel 2").receive((msg) => {
-    t.is(msg, "reply from channel:2 - your payload was: payload channel 2");
+    t.is(
+      msg,
+      `reply from ${channelName2} - your payload was: payload channel 2`
+    );
     channel2.push("push from 2").receive((msg) => {
-      t.is(msg, "reply on push from channel:2 - your payload was: push from 2");
+      t.is(
+        msg,
+        `reply on push from ${channelName2} - your payload was: push from 2`
+      );
     });
   });
 
@@ -92,17 +123,18 @@ test.cb(
     t.plan(2);
     const socket1 = new Socket("ws://localhost:8080/ws");
     const socket2 = new Socket("ws://localhost:8080/ws");
+    const channelName1 = newChannel();
 
     socket1.connect();
     socket2.connect();
 
-    const channel1_1 = socket1.channel("channel:1");
-    const channel1_2 = socket2.channel("channel:1");
+    const channel1_1 = socket1.channel(channelName1);
+    const channel1_2 = socket2.channel(channelName1);
     channel1_1.on((msg) => {
-      t.is(msg, "broadcast from channel:1");
+      t.is(msg, `broadcast from ${channelName1}`);
     });
     channel1_2.on((msg) => {
-      t.is(msg, "broadcast from channel:1");
+      t.is(msg, `broadcast from ${channelName1}`);
     });
     channel1_1.join("payload channel 1").receive(() => {
       channel1_2.join("payload channel 2").receive(() => {
@@ -116,23 +148,22 @@ test.cb(
 test.cb("only the joined channel receives the broadcast", (t) => {
   t.plan(1);
   const socket = new Socket("ws://localhost:8080/ws");
+  const channelName1 = newChannel();
+  const channelName2 = newChannel();
 
   socket.connect();
 
-  const channel1 = socket.channel("channel:1");
-  const channel2 = socket.channel("channel:2");
-  const channel3 = socket.channel("channel:3");
+  const channel1 = socket.channel(channelName1);
+  const channel2 = socket.channel(channelName2);
   channel1.on((msg) => {
-    t.is(msg, "broadcast from channel:1");
+    t.is(msg, `broadcast from ${channelName1}`);
   });
   channel2.on((msg) => {
     t.fail(`${msg}`);
   });
   channel1.join("payload channel 1").receive(() => {
     channel2.join("payload channel 2").receive(() => {
-      channel3.join("payload channel 3").receive(() => {
-        channel1.push("broadcast");
-      });
+      channel1.push("broadcast");
     });
   });
   end(t);
@@ -140,11 +171,12 @@ test.cb("only the joined channel receives the broadcast", (t) => {
 
 test("cannot join the same channel twice - in phoenix they close the old channel and start the new one", (t) => {
   const socket = new Socket("ws://localhost:8080/ws");
+  const channelName1 = newChannel();
 
-  const channel1 = socket.channel("channel:1");
+  const channel1 = socket.channel(channelName1);
   t.throws(
     () => {
-      socket.channel("channel:1");
+      socket.channel(channelName1);
     },
     { instanceOf: Error }
   );
