cbb9145d9d70e42af28383bc5262be63a6c71f65
Thomas Coopman <thomas.coopman@gmail.com>
2021-07-19 23:17:05 +0200

    try jsoo with gen_js_api

--- jsoo/main.ml
+++ jsoo/main.ml
@@ -1,4 +1,9 @@
+module Console = [%js:
+  val log: string -> unit [@@js.global "console.log"]
+]
+
 let () =
   let _ = 1 + 2 in
   let () = Stdlib.print_endline "test" in
+  Console.log "foobar";
   ()
