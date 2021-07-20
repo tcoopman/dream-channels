ba02f7c2ace2a10cfa957a06d7337b96854d4ea1
Thomas Coopman <thomas.coopman@gmail.com>
2021-07-20 08:46:51 +0200

    use ocaml-vdom from LexiFi

--- jsoo/main.ml
+++ jsoo/main.ml
@@ -1,9 +1,42 @@
-module Console = [%js:
-  val log: string -> unit [@@js.global "console.log"]
-]
+(* This file is part of the ocaml-vdom package, released under the terms of an MIT-like license.     *)
+(* See the attached LICENSE file.                                                                    *)
+(* Copyright 2016 by LexiFi.                                                                         *)
 
-let () =
-  let _ = 1 + 2 in
-  let () = Stdlib.print_endline "test" in
-  Console.log "foobar";
-  ()
+(* Inspired from https://github.com/janestreet/incr_dom/blob/master/example/incr_decr/counters.ml *)
+
+open Vdom
+
+module IntMap = Map.Make (struct
+  type t = int
+
+  let compare : int -> int -> int = compare
+end)
+
+type model = { counters : int IntMap.t }
+
+let update { counters } = function
+  | `New_counter ->
+      { counters = IntMap.add (IntMap.cardinal counters) 0 counters }
+  | `Update (pos, diff) ->
+      { counters = IntMap.add pos (IntMap.find pos counters + diff) counters }
+
+
+let init = { counters = IntMap.empty }
+
+let button txt msg = input [] ~a:[ onclick (fun _ -> msg); type_button; value txt ]
+
+let view { counters } =
+  let row (pos, value) =
+    div
+      [ button "-" (`Update (pos, -1)); text (string_of_int value); button "+" (`Update (pos, 1)) ]
+  in
+  div (div [ button "Ne couner" `New_counter ] :: (IntMap.bindings counters |> List.map row))
+
+
+let app = simple_app ~init ~view ~update ()
+
+open Js_browser
+
+let run () = Vdom_blit.run app |> Vdom_blit.dom |> Element.append_child (Document.body document)
+
+let () = Window.set_onload window run
