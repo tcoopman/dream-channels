fc4a68d64c9fef894acca3badbd0adba93cff474
Thomas Coopman <thomas.coopman@gmail.com>
2021-07-20 11:45:37 +0200

    add dream-channels with dummy implementation

--- examples/chat/server/server.eml.ml
+++ examples/chat/server/server.eml.ml
@@ -1,3 +1,36 @@
+open Dream_channels
+
+let ok () = Lwt.return `Ok
+
+let reply answer = Lwt.return (`Reply answer)
+
+let stop reason = Lwt.return (`Stop reason)
+
+let chat_channel =
+  Socket.
+    { topic = "chat:*"
+    ; intercept = (function _ -> false)
+    ; create_callbacks =
+        (fun topic ->
+          { join =
+              (fun _functions (Payload payload) ->
+                match (topic, payload) with
+                | WithSubtopic (_, _chat_id), _ ->
+                    ok ()
+                | _ ->
+                    stop "invalid topic" )
+          ; handle_message =
+              (fun _functions (Payload payload) ->
+                match (topic, payload) with
+                | WithSubtopic (_, _chat_id), _ ->
+                    ok ()
+                | _ ->
+                    stop "invalid topic" )
+          ; handle_out = (fun (Payload _) -> None)
+          ; terminate = (fun () -> ())
+          } )
+    }
+
 let home =
   <html>
     <body id="body">
@@ -6,12 +39,16 @@ let home =
   </html>
 
 let () =
-  Dream.run
+  Dream.initialize_log ~level:`Debug () ;
+  Dream.run ~interface:"0.0.0.0"
   @@ Dream.logger
   @@ Dream.router [
 
     Dream.get "/"
       (fun _ -> Dream.html home);
+
+    Dream.get "/ws" (fun _ ->
+      Dream.websocket @@ Socket.channels [ chat_channel ] );
 
     Dream.get "/static/**"
       (Dream.static "./static");
