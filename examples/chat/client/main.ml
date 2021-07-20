ecd5d85b7c3c58baf9cb92ad098757e877658077
Thomas Coopman <thomas.coopman@gmail.com>
2021-07-20 20:20:18 +0200

    formatting

--- examples/chat/client/main.ml
+++ examples/chat/client/main.ml
@@ -10,60 +10,74 @@ module IntMap = Map.Make (struct
   let compare : int -> int -> int = compare
 end)
 
-type 'msg Vdom.Cmd.t +=
-  | Run of (('msg -> unit) -> unit)
+type 'msg Vdom.Cmd.t += Run of (('msg -> unit) -> unit)
 
 module ChatChannel = struct
-  type model = {
-    joined: bool;
-    channel: Ws.Channel.t;
-    messages: string list;
-    text: string;
-  }
+  type model =
+    { joined : bool
+    ; channel : Ws.Channel.t
+    ; messages : string list
+    ; text : string
+    }
 
-  let init = 
+  let init =
     let socket = Ws.Socket.create "ws://localhost:8080/ws" in
     let () = Ws.Socket.connect socket in
     let channel = Ws.Socket.channel socket "chat:1" in
-    return {joined = false; channel; messages = []; text = ""}
+    return { joined = false; channel; messages = []; text = "" }
+
 
   let update model = function
-    | `Join -> return model ~c:[Run (fun send -> 
-        let channel = model.channel in
-        let joinReceive = Ws.Channel.join channel "Thomas" in
-        Ws.Channel.on channel (fun msg -> send (`Received msg));
-        Ws.ChannelPush.receive joinReceive (fun msg -> send (`Joined msg))
-        )]
-    | `Joined message -> return {model with joined = true; messages = message :: model.messages}
-    | `Received message -> return {model with messages = message :: model.messages}
-    | `Text text -> return {model with text}
-    | `Send -> 
+    | `Join ->
+        return
+          model
+          ~c:
+            [ Run
+                (fun send ->
+                  let channel = model.channel in
+                  let joinReceive = Ws.Channel.join channel "Thomas" in
+                  Ws.Channel.on channel (fun msg -> send (`Received msg)) ;
+                  Ws.ChannelPush.receive joinReceive (fun msg -> send (`Joined msg)) )
+            ]
+    | `Joined message ->
+        return { model with joined = true; messages = message :: model.messages }
+    | `Received message ->
+        return { model with messages = message :: model.messages }
+    | `Text text ->
+        return { model with text }
+    | `Send ->
         let () = ignore (Ws.Channel.push model.channel model.text) in
-        return {model with text = ""}
+        return { model with text = "" }
+
 
   let view model =
-    if not model.joined then
-      button "Join" `Join
+    if not model.joined
+    then button "Join" `Join
     else
-      div [
-        div (List.map (fun message -> div [text message]) model.messages);
-        input [] ~a:[oninput (fun s -> `Text s); value model.text];
-        button "Send" `Send
-      ]
+      div
+        [ div (List.map (fun message -> div [ text message ]) model.messages)
+        ; input [] ~a:[ oninput (fun s -> `Text s); value model.text ]
+        ; button "Send" `Send
+        ]
 
-  let app = {init; update; view}
+
+  let app = { init; update; view }
 end
 
-  let cmd_handler ctx = function
-    | Run cb ->
-        cb (fun x -> Vdom_blit.Cmd.send_msg ctx x);
-        true
-    | _ -> false
+let cmd_handler ctx = function
+  | Run cb ->
+      cb (fun x -> Vdom_blit.Cmd.send_msg ctx x) ;
+      true
+  | _ ->
+      false
 
-let () = Vdom_blit.(register (cmd {f = cmd_handler}))
+
+let () = Vdom_blit.(register (cmd { f = cmd_handler }))
 
 open Js_browser
 
-let run () = Vdom_blit.run (ChatChannel.app) |> Vdom_blit.dom |> Element.append_child (Document.body document)
+let run () =
+  Vdom_blit.run ChatChannel.app |> Vdom_blit.dom |> Element.append_child (Document.body document)
+
 
 let () = Window.set_onload window run
--- examples/chat/client/ws.mli
+++ examples/chat/client/ws.mli
@@ -1,9 +1,11 @@
-module[@js.scope "Channels"] ChannelPush : sig
+module ChannelPush : sig
   type t = private Ojs.t
+
   val receive : t -> (string -> unit) -> unit [@@js.call]
 end
+[@@js.scope "Channels"]
 
-module[@js.scope "Channels"] Channel : sig
+module Channel : sig
   type t = private Ojs.t
 
   val join : t -> string -> ChannelPush.t [@@js.call]
@@ -12,12 +14,15 @@ module[@js.scope "Channels"] Channel : sig
 
   val push : t -> string -> ChannelPush.t [@@js.call]
 end
-module[@js.scope "Channels"] Socket : sig
+[@@js.scope "Channels"]
+
+module Socket : sig
   type t = private Ojs.t
 
-  val create: string -> t [@@js.new "Socket"]
+  val create : string -> t [@@js.new "Socket"]
 
-  val connect: t -> unit [@@js.call]
+  val connect : t -> unit [@@js.call]
 
   val channel : t -> string -> Channel.t [@@js.call]
 end
+[@@js.scope "Channels"]
